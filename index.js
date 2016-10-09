8c61daa618b02e15d700bafe3aec1db775123157
Dylan Piercey <pierceydylan@gmail.com>
2016-10-09 11:34:46 -0700

    Fix withCredentials option

--- index.js
+++ index.js
@@ -55,8 +55,11 @@ module.exports = function fetcherMiddleware (config) {
 
       // Automatically send credentials if `withCredentials` is enabled.
       if (config.withCredentials && !opts.credentials) {
-        opts.credentials = 'same-origin'
-        opts.headers.set('Cookie', ctx.req.get('Cookie'))
+        var cookie = ctx.req.get('cookie')
+        if (cookie) {
+          opts.credentials = 'same-origin'
+          opts.headers.set('Cookie', ctx.req.get('Cookie'))
+        }
       }
 
       // Allow event handlers to modify request options.
