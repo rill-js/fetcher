d4a69986277c90f9fd2508aff95e20d43c57a201
Dylan Piercey <pierceydylan@gmail.com>
2016-10-21 12:59:58 -0700

    Only send x-forwarded-for if we are server side

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ module.exports = function fetcherMiddleware (config) {
       opts = opts || {}
       opts.headers = toHeaders(opts.headers)
 
-      // Forward current ip address with request.
-      if (opts.forwardIP && !opts.headers.get('X-Forwarded-For')) {
+      // Forward current ip address with request if we are server side.
+      if (!process.browser && opts.forwardIP && !opts.headers.get('X-Forwarded-For')) {
         opts.headers.set('X-Forwarded-For', ctx.req.ip)
       }
 
